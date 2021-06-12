384d8f988329607cc8219635230bc0bcde420372
slayerprogrammer <tolgahanozturk0635@gmail.com>
2021-06-12 13:09:52 +0300

    application duzenleme

--- src/main/java/com/hrms/applicationhrms/ApplicationHrms.java
+++ src/main/java/com/hrms/applicationhrms/ApplicationHrms.java
@@ -2,12 +2,27 @@ package com.hrms.applicationhrms.applicationhrms;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.bind.annotation.CrossOrigin;
+import springfox.documentation.builders.RequestHandlerSelectors;
+import springfox.documentation.spi.DocumentationType;
+import springfox.documentation.spring.web.plugins.Docket;
+import springfox.documentation.swagger2.annotations.EnableSwagger2;
 
 @SpringBootApplication
-public class ApplicationHrms {
+@EnableSwagger2
+public class HrmsProjectApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApplicationHrms.class, args);
+		SpringApplication.run(HrmsProjectApplication.class, args);
+	}
+
+	@Bean
+	public Docket api() {
+		return new Docket(DocumentationType.SWAGGER_2)
+				.select()
+				.apis(RequestHandlerSelectors.basePackage("hrms.hrmsProject"))
+				.build();
 	}
 
 }
